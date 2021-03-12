8d64d4d4ad0ff6a127173ecb3d5f33d696ff47ff
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-12 14:30:05 -0300

    DTOs para entrada e saída de pessoa na api

---
+++ src/main/java/br/com/softplan/selecao/api/dto/PessoaCadastroDTO.java
@@ -0,0 +1,103 @@
+package br.com.softplan.selecao.api.dto;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.lang.Nullable;
+
+public class PessoaCadastroDTO {
+
+	@NotBlank
+	private String nome;
+
+	@Nullable
+	private String sexo;
+	
+	@Email
+	private String email;
+	
+	@NotNull
+	private LocalDate dataNascimento;
+	
+	@Nullable
+	private String naturalidade;
+
+	@Nullable
+	private String nacionalidade;
+
+	@CPF
+	@NotBlank
+	private String cpf;
+
+	public PessoaCadastroDTO(String nome, String sexo, String email, LocalDate dataNascimento, String naturalidade,
+			String nacionalidade, String cpf) {
+		this.nome = nome;
+		this.sexo = sexo;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.naturalidade = naturalidade;
+		this.nacionalidade = nacionalidade;
+		this.cpf = cpf;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+	
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
---
+++ src/main/java/br/com/softplan/selecao/api/dto/PessoaRetornoDTO.java
@@ -0,0 +1,116 @@
+package br.com.softplan.selecao.api.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class PessoaRetornoDTO {
+	
+	private Integer id;
+	private String nome;
+	private String sexo;
+	private String email;
+	private LocalDate dataNascimento;
+	private String naturalidade;
+	private String nacionalidade;
+	private String cpf;
+	private LocalDateTime dataCriacao;
+	private LocalDateTime dataAtualizacao;
+	
+	public PessoaRetornoDTO() {
+	}
+	
+	public PessoaRetornoDTO(Integer id, String nome, String sexo, String email, LocalDate dataNascimento,
+			String naturalidade, String nacionalidade, String cpf, LocalDateTime dataCriacao,
+			LocalDateTime dataAtualizacao) {
+		this.id = id;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.naturalidade = naturalidade;
+		this.nacionalidade = nacionalidade;
+		this.cpf = cpf;
+		this.dataCriacao = dataCriacao;
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+	
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	public LocalDateTime getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	
+	public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+}
