26ac9898b10b4cc7e84c1cc0ecd0a15a95fb1e64
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-13 01:00:34 -0300

    removido specification sem necessidade

--- src/main/java/br/com/softplan/selecao/api/repository/PessoaRepository.java
+++ src/main/java/br/com/softplan/selecao/api/repository/PessoaRepository.java
@@ -1,10 +1,9 @@
 package br.com.softplan.selecao.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import br.com.softplan.selecao.api.model.Pessoa;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer>, JpaSpecificationExecutor<Pessoa> {
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
 }
