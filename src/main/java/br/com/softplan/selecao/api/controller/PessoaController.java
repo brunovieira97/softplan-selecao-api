c0e8a265e16edb9694b754247ca33a10d1f9d4ea
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-15 23:08:50 -0300

    cors (#4)

--- src/main/java/br/com/softplan/selecao/api/controller/PessoaController.java
+++ src/main/java/br/com/softplan/selecao/api/controller/PessoaController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import br.com.softplan.selecao.api.exception.ResourceNotFoundException;
 import br.com.softplan.selecao.api.service.PessoaService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/pessoas")
 public class PessoaController {
 
