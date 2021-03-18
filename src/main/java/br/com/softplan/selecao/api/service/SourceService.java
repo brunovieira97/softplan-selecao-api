91d82af1eddfaeb2846de485f4246927d29d5e24
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-18 01:36:50 -0300

    endpoint para link do repositório (#2)
    * endpoint de link github
    
    * atualizado readme
    
    * removida paginação para simplificar implementação do front (#3)
    
    * Update README.md
    
    * cors (#4)
    
    * autenticação (#5)
    
    * adicionado spring security
    
    * prefixo api + usuário/senha de login
    
    * endpoint para teste de login
    
    * config de cors completa
    
    * config de security
    
    * endpoint de link github
    
    * endpoint de url sem auth

---
+++ src/main/java/br/com/softplan/selecao/api/service/SourceService.java
@@ -0,0 +1,11 @@
+package br.com.softplan.selecao.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SourceService {
+	
+	public String getProjectUrl() {
+		return "https://github.com/brunovieira97/softplan-selecao-api";
+	}
+}
