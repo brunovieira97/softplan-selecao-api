a484c71c7dc499838aed630f45660a068f5f6e72
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-18 01:23:28 -0300

    config de cors completa

---
+++ src/main/java/br/com/softplan/selecao/api/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package br.com.softplan.selecao.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	@Primary
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource urlSource = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+
+		corsConfig.setAllowCredentials(true);
+		corsConfig.addAllowedOrigin("http://localhost:4200");
+		corsConfig.addAllowedHeader("*");
+		
+		corsConfig.addAllowedMethod("OPTIONS");
+		corsConfig.addAllowedMethod("HEAD");
+		corsConfig.addAllowedMethod("GET");
+		corsConfig.addAllowedMethod("PUT");
+		corsConfig.addAllowedMethod("POST");
+		corsConfig.addAllowedMethod("DELETE");
+		corsConfig.addAllowedMethod("PATCH");
+		
+		urlSource.registerCorsConfiguration("/**", corsConfig);
+		
+		return new CorsFilter(urlSource);
+	}
+}
--- src/main/java/br/com/softplan/selecao/api/controller/PessoaController.java
+++ src/main/java/br/com/softplan/selecao/api/controller/PessoaController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import br.com.softplan.selecao.api.exception.ResourceNotFoundException;
 import br.com.softplan.selecao.api.service.PessoaService;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/pessoas")
 public class PessoaController {
 
